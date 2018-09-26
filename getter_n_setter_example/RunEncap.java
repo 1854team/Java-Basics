daeac0500130f055971d27fa739818ef419c6219
Larry Savidge <savidge00@gmail.com>
2018-09-25 20:40:39 -0400

    Create RunEncap.java

---
+++ getter_n_setter_example/RunEncap.java
@@ -0,0 +1,11 @@
+public class RunEncap {
+
+    public static void main(String rgs[]) {
+        EncapTest encap = new EncapTest();     //creates new instance of EncapTest
+        encap.setName("James");                      //user can create new name for private name variable
+        encap.setAge(20);
+        encap.setIdNum("12343ms");
+
+        System.out.println("Name: " encap.getName() + "Age: " encap.getAge());
+    }
+}
